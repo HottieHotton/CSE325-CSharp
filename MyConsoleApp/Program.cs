9e8273599f64f24c6f25d8990464bc82198ba670
Braxton Hotton <bhotton25@gmail.com>
2025-03-08 19:32:06 -0700

    Finished today's modules for Assignment 1

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"The current time is {DateTime.Now}");
+Console.WriteLine($"There are {new DateTime(2025, 12, 25).DayOfYear - DateTime.Now.DayOfYear} days left till Christmas!");
